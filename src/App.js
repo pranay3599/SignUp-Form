20d7a6d5ed1eafecfe7cefbafaffbb4e658cd469
pranay3599 <pranaymangrudkar03@gmail.com>
2023-12-16 21:23:08 +0530

    submitted

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUpForm from './component/SignUpForm';
+
+
+function App() {
+  return (
+    <SignUpForm></SignUpForm>
+  );
+}
+
+export default App;
